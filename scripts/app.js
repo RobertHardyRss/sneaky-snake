0da6066bfe44d88e72357b3e9fad4882ad901c30
Robert Hardy <rhardy1@dcsdk12.org>
2022-11-30 16:24:16 -0700

    Added head

--- scripts/app.js
+++ scripts/app.js
@@ -15,13 +15,22 @@ class Player {
 	/**
 	 * @param {number} x
 	 * @param {number} y
+	 * @param {CanvasRenderingContext2D} ctx
 	 */
-	constructor(x, y) {
+	constructor(x, y, ctx) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
-		this.segments = [];
+		this.ctx = ctx;
+		this.segments = [new Segment(this.x, this.y, "yellow", this.ctx)];
+	}
+
+	update() {}
+	draw() {
+		this.segments.forEach((s) => {
+			s.draw();
+		});
 	}
 }
 
@@ -30,16 +39,40 @@ class Segment {
 	 * @param {number} x
 	 * @param {number} y
 	 * @param {string} color
+	 * @param {CanvasRenderingContext2D} ctx
 	 */
-	constructor(x, y, color) {
+	constructor(x, y, color, ctx) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
 		this.color = color;
+		this.ctx = ctx;
 	}
 
 	update() {}
 
-	draw() {}
+	draw() {
+		this.ctx.fillStyle = this.color;
+		this.ctx.fillRect(this.x, this.y, this.w, this.h);
+	}
 }
+
+let p1 = new Player(
+    5 * game.gridSize,
+    5 * game.gridSize,
+    ctx);
+
+let currentTime = 0;
+
+function gameLoop(timestamp) {
+	let elapsedTime = timestamp - currentTime;
+	currentTime = timestamp;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	p1.draw();
+
+	requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
